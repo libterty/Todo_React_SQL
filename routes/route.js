d513f6f99db07472739d20a044ef5d56c2062993
dreak603 <dreak603@gmail.com>
2019-10-02 00:01:14 +0800

    add home and user routers

--- routes/route.js
+++ routes/route.js
@@ -6,32 +6,51 @@ const Todo = db.Todo;
 const User = db.User;
 
 router.get('/', (req, res) => {
-  res.send('hello world');
+    res.send('hello world');
 });
 // 認證系統的路由
 // 登入頁面
 router.get('/users/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 // 登入檢查
 router.post('/users/login', (req, res) => {
-  res.send('login');
+    res.send('login');
 });
 // 註冊頁面
 router.get('/users/register', (req, res) => {
-  res.render('register');
+    res.render('register');
 });
 // 註冊檢查
 router.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'));
+    const { name, email, password, password2 } = req.body;
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user) {
+            console.log(`${user} are already an user.`);
+            res.status(200).render('register', {
+                name,
+                email,
+                password,
+                password2
+            });
+        } else {
+            const newUser = new User({
+                name,
+                email,
+                password
+            });
+            newUser
+                .save()
+                .then(user => res.status(200).redirect('/'))
+                .catch(err =>
+                    res.status(400).send(`Bad request, error Message: ${err.message}`)
+                );
+        }
+    });
 });
 // 登出
 router.get('/users/logout', (req, res) => {
-  res.send('logout');
+    res.send('logout');
 });
 
 module.exports = router;
